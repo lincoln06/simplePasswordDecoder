8ab1203012ee876bcd182651a5b3ec6e408d6d05
Maciej Janta-Lipiński <maciekjl993@gmail.com>
2022-08-04 16:36:32 +0200

    100% working

--- passwordMaker/Program.cs
+++ passwordMaker/Program.cs
@@ -1,79 +1,46 @@
-﻿string password = String.Empty;
+﻿using System.Text;
+
+string password=String.Empty;
 bool isValid = false;
-while (password == String.Empty || password.Length<8 || !isValid)
+while (password.Length<8 || !isValid)
 {
-    Console.WriteLine("Podaj hasło. Minimum 8 znaków, jedna cyfra, jedna wielka litera i znak specjalny");
+    Console.WriteLine("Podaj hasło. Minimum 8 znaków, jedna wielka litera i jedna cyfra");
     password = Console.ReadLine();
-    isValid = Validate(password);
+    isValid = CheckPass(password);
 }
 
 Console.WriteLine($"Hasło przed zaszyfrowaniem: {password}");
-password = Encrypt(password);
-Console.WriteLine($"Hasło po zaszyfrowaniu: {password}");
-password = Decrypt(password);
-Console.WriteLine($"Hasło po ponownym odszyfrowaniu: {password}");
+password = MakePassword(password,1);
+Console.WriteLine($"Hasło po zaszyfrowaniu: {password}\nNaciśnij dowolny klawisz...");
+password = MakePassword(password, -1);
+Console.ReadKey();
+Console.WriteLine($"Hasło po odszyfrowaniu: {password}");
+
 Console.ReadKey();
 
+static string MakePassword(string password,int number)
+{
+    int[] numbersArray = new int[password.Length];
+    char[] signsArray = new char[password.Length];  
+    for (int i = 0; i < password.Length; i++)
+    {
+        signsArray[i] = password[i];
+        numbersArray[i] = (int)signsArray[i];
+        signsArray[i] = (char)(numbersArray[i] + number);
+    }
 
-static bool Validate(string password)
+    return new string(signsArray);
+}
+
+static bool CheckPass(string password)
 {
     bool firstTerm = false;
     bool secondTerm = false;
-    bool thirdTerm = false;
     foreach (char sign in password)
     {
-        if (Char.IsUpper(sign)) firstTerm = true;
-        if (Char.IsDigit(sign)) secondTerm = true;
-        if (!Char.IsWhiteSpace(sign)) thirdTerm = true;
-    }
-    Console.WriteLine($"1:{firstTerm}2:{secondTerm}3:{thirdTerm}\n");
-    if (firstTerm && secondTerm && thirdTerm) return true;
-    return false;
-}
-
-static string Encrypt(string password)
-{
-    int[] passArray=MakeItArray(password);
-    for (int i = 0; i < passArray.Length; i++)
-    {
-        passArray[i] += 1;
+        if (Char.IsDigit(sign)) firstTerm = true;
+        if (Char.IsUpper(sign)) secondTerm = true;
     }
 
-    char[] tempArray = new char[passArray.Length];
-    for (int i = 0; i < passArray.Length; i++)
-    {
-        tempArray[i] = (char)passArray[i];
-    }
-
-    password = tempArray.ToString();
-    return password;
-}
-
-static string Decrypt(string password)
-{
-    int[] passArray=MakeItArray(password);
-    for (int i = 0; i < passArray.Length; i++)
-    {
-        passArray[i]-=1;
-    }
-
-    
-    
-
-    password = tempArray.ToString();
-    return password;
-}
-
-static int[] MakeItArray(string password)
-{
-    char[] passArray = password.ToCharArray();
-    int[] tempArray = new int[passArray.Length];
-    for (int i = 0; i < passArray.Length; i++)
-    {
-        tempArray[i] = (int)passArray[i];
-        Console.WriteLine($"{tempArray[i]}\t{passArray[i]}");
-    }
-
-    Console.ReadKey();
-    return tempArray;
+    return firstTerm && secondTerm;
 }
